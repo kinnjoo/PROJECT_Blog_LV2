175ae193430f87442005a6161a94ab71bcb521ba
Kim Joohee <syap1170@gmail.com>
2023-06-21 11:35:59 +0900

    회원가입 API(추가 수정 필요)
    닉네임 조건 추가, 패스워드 조건 수정 & 암호화

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const port = 3000;
 
 const postsRouter = require("./routes/posts.js");
 const commentsRouter = require("./routes/comments.js");
+const usersRouter = require("./routes/users.js");
 
 const connent = require("./schemas");
 connent();
 
 app.use(express.json());
 
-app.use("/", [postsRouter, commentsRouter]);
+app.use("/", [postsRouter, commentsRouter, usersRouter]);
 
 app.listen(port, () => {
   console.log(port, "포트로 서버가 열렸어요!");
--- schemas/index.js
+++ schemas/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = () => {
   mongoose
-    .connect("mongodb://localhost:27017/PROJECT_Blog_LV1")
+    .connect("mongodb://localhost:27017/PROJECT_Blog_LV2")
     .catch(err => console.log(err));
 };
 
